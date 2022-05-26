aa539bdadf3c005ae34c30065a4a9d1d82f0d74c
Max Reshetnikov <maxim.a.reshetnikov@gmail.com>
2022-05-26 21:50:20 +0400

    homework-3 ver 1

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,9 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+	result = make([]int64, len(input), cap(input))
+	for i, v := range input {
+		result[len(input)-i-1] = v
+	}
+	return result
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,20 @@
 package homework
 
+import (
+	"sort"
+)
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	return
+	l := len(input)
+	result = make([]string, 0, l)
+
+	keys := make([]int, 0, l)
+	for k := range input {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, d := range keys {
+		result = append(result, input[d])
+	}
+	return result
 }
